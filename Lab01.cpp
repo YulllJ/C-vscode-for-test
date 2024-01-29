3f2b4d9d4856fbc1f97055728d6a4b0bea037405
YulllJ <jyr8407@gmail.com>
2024-01-29 16:16:51 +0900

    4th commit vscode c++

--- Lab01.cpp
+++ Lab01.cpp
@@ -1,233 +1,45 @@
-/*
 #include<iostream>
+#include<fstream>
+#include<iomanip>
 using namespace std;
+
 int main(){
-    int score1, score2, score3, maxScore, minScore, score;
-    cout<<"첫번째 정수입력:";
-    cin>>score1;
-    cout<<"두번째 정수입력:";
-    cin>>score2;
-    cout<<"세번째 정수입력:";
-    cin>>score3;
-    if(score1>score2 && score1>score3){
-        maxScore=score1;
+
+    const int CAPACITY = 10;
+    int frequencies[CAPACITY] = {0};
+    ifstream integerFile;
+
+    integerFile.open("integerFile.dat");
+    if(!integerFile){
+        cout <<"숫자 파일을 열 수 없습니다."<<endl;
+        cout <<"프로그램을 중단합니다." ;
+        return 0;
     }
-    else if(score2>score1 && score2>score3){
-        maxScore=score2;
+    // BOM 제거
+    char bom[3];
+    integerFile.read(bom, 3);
+    if (!(bom[0] == (char)0xEF && bom[1] == (char)0xBB && bom[2] == (char)0xBF)) {
+        // BOM이 아니라면 파일 포인터를 처음으로 되돌림
+        integerFile.seekg(0);
     }
-    else{
-        maxScore=score3;
-    }
-
-
-     if(score1<score2 && score1<score3){
-        minScore=score1;
-    }
-    else if(score2<score1 && score2<score3){
-        minScore=score2;
-    }
-    else{
-        minScore=score3;
-    }
-    int temp =maxScore+minScore;
-    if(temp % 2 ==1){
-        temp+=1;
-
-    }
-    score=temp/2;
-
-    cout<<"입력한 점수="<<score1<<" "<<score2<<" "<<score3<<endl;
-    cout<<"최고점수와 최대점수= ";
-    cout<<minScore<< ' '<<maxScore<<endl;
-    cout<<"학생성적="<<score<<endl;
-
-    return 0;
-}
-#include <iostream>
-using namespace std;
-int main()
-{
-    double income, tax;
-    bool bracket1, bracket2, bracket3, bracket4;
-    double limit1 = 10000.00, limit2 = 50000.00, limit3 = 100000.00;
-    double rate1 = 0.05, rate2 = 0.10, rate3 = 0.15, rate4 = 0.20;
-
-    cout << "수입을 달러로 입력하세여 :";
-    cin >> income;
-    bracket1 = (income <= limit1) && (income >= 0);
-    bracket2 = (income > limit1) && (income <= limit2);
-    bracket3 = (income > limit2) && (income <= limit3);
-    bracket4 = (income > limit3);
-
-    if (bracket1)
-    {
-        tax = income * rate1;
-    }
-    else if (bracket2)
-    {
-        tax = limit1 * rate1 + (income - limit1) * rate2;
-    }
-    else if (bracket3)
-    {
-        tax = limit1 * rate1 + (limit2 - limit1) * rate2 + (income - limit2) * rate3;
-    }
-    else if (bracket4)
-    {
-        tax = limit1 * rate1 + (limit2 - limit1) * rate2 + (income - limit2) * rate3;
-    }
-}
-
-//shift alt f:자동 줄맞춤
-
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
-
-using namespace std;
-
-string test()//type이 vector string임 string하고 달라
-{
-    vector<string> str;
-    for (int i = 0; i < 26; i++)
-    {
-        str.push_back(string(1, static_cast<char>('a' + i)));
-    }
-    string combinedString;
-    for (const string& str : str) {
-        combinedString += str;
-    }
-    return combinedString;
-}
-int main()
-{
-    string a; // 대문자 ㅁsms 65 소문자 97
-
-    a=test();
-    for (int i = 0; i < size(a); i++)
-    {
-
-        if (a[i] >= 'a' && a[i] <= 'z')
-        {
-            cout << static_cast<char>(a[i] - 32) << " ";
-        }
-
-
-    }
-    return 0;
-}*/
-/*
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-int main()
-{
-    int startDay;
-    int daysInMonth;
-    int col = 1;
-    do
-    {
-        cout << "한달의 날짜수를 입력하세여(28,29,30,31): ";
-        cin >> daysInMonth;
-    } while (daysInMonth < 28 || daysInMonth > 31);
-
-    do
-    {
-        cout << "첫날의 요일을 입력하세요(0~6): ";
-        cin >> startDay;
-    } while (startDay < 0 || startDay > 6);
-
-    cout << endl
-         << "Sun Mon Tue Wed Thr Fru Sat" << endl
-         << "--- --- --- --- --- --- --- " << endl;
-    for (int space = 0; space < startDay; space++)
-    {
-        cout << "    "; // 날짜 하나에 해당하는 공백
-        col++;
-    }
-    for (int day = 1; day <= daysInMonth; day++)
-    {
-        cout << setw(3) << day << " ";
-        col++;
-        if (col > 7)
-        {
-            cout << endl;
-            col = 1;
+    int data ;
+    int size = 0;
+    while(integerFile >> data){
+        if(data>=0 && data<=9){
+            size++;
+            frequencies[data]++;
         }
     }
-}
+    integerFile.close();
 
-#include <iostream>
-#include <cmath>
-using namespace std;
+    cout << "파일 안에 " << size << "개의 유효한 데이터가 있습니다." << endl;
 
-int main()
-{
-    const double PI = 3.141529635897932346;
-    int n;
-    double s, peri, area;
-
-    do
-    {
-        cout << "변의 개수를 입력하세요(4이상의 정수):";
-        cin >> n;
-    } while (n < 4);//while 만족하면 동작 안하는거인듯
-
-        do
-    {
-        cout << "변의 길이를 입력하세요:";
-        cin >> s;
+    for(int i =0; i<10; i++){
+        cout << setw(3) << i << " ";
+        for(int f=1; f<=frequencies[i]; f++){
+            cout <<'*';
+        }
+        cout<< " " << frequencies[i]<< endl;
     }
-    while (s <= 0.0) ;
-    peri = n * s;
-    area = (n * pow(s, 2)) / (n * tan(PI / n));
-    cout << "둘레: " << peri << endl;
-    cout << "넓이: " << area;
-    return 0;
-}*/
-// 7-3
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-class RandomInteger
-{
-private:
-    int low;
-    int high;
-    int value;
-
-public:
-    RandomInteger(int low, int high);
-    ~RandomInteger();
-    RandomInteger(const RandomInteger &random) = delete;
-    void print() const;
-};
-RandomInteger ::RandomInteger(int lw, int hh)
-    : low(lw), high(hh)
-{
-    srand(time(0));
-    int temp = rand();
-    value = temp % (high - low + 1) + low;
-}
-RandomInteger::~RandomInteger() {}
-void RandomInteger::print() const
-{
-    cout << value << endl;
-}
-
-int main()
-{
-    RandomInteger r1(100, 200);
-    cout << "100~200사이의 랜덤한 함수 숫자: ";
-    r1.print();
-    RandomInteger r2(400, 600);
-    cout << "400~600사이의 랜덤한 함수 숫자: ";
-    r2.print();
-    RandomInteger r3(1200, 2000);
-    cout << "1500~2000사이의 랜덤한 함수 숫자: ";
-    r3.print();
     return 0;
 }
