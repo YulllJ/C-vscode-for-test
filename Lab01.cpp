401633284476b3b79ab171ada5a46dbdd48dc09f
YulllJ <jyr8407@gmail.com>
2024-01-30 15:09:38 +0900

    8th commit vscode c++-const

--- Lab01.cpp
+++ Lab01.cpp
@@ -1,22 +1,47 @@
-
-//const 쓰면 처음 할당 이후로 값을 바꿀수 없어서 보호가 가능함 함수에 const를 붙이면 리턴값을 상수화시켜서 보호한다는 의미(이후에 값을 바꾸면 컴파일 에러남)
-//const int num= function() 에서 function이 const일때는 reference를 사용할때 의미가 있는 것이다.<-- 이해 못함 함수 자체에 const붙이는게 왜 reference랑 있어야 한다는겨
-//함수에서 안에서 초기화할때는 function():num(3)이런식으로위에 코드에도 있듯이 이런식으로 수정해주는 거인듯
-//const로 오버로딩가능(함수 구분이 가능해진다는 의미)
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-/*
-const int& Function(){
-    int& num1 = n;
-    return num1;
-}*/
-const int Function(){return 10;}
-int main(){
-    int num1= Function();
-    const int num2=Function();
-    //int& num2 = Function();
-    //const int& num3 = Function();
-    num1=30;
-    num2=30;
 
+int main()
+{
+    int maxPower = 10;
+    int n;
+
+    do
+    {
+        cout << "(x+y)^n에서 n을 입력하세요: " << endl;
+        cin >> n;
+    } while (n < 0 || n > maxPower);
+
+    int **pascal = new int *[n + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        pascal[i] = new int[i];
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            if (j == 0 || i == j)
+            {
+                pascal[i][j] = 1;
+            }
+            else
+            {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+        }
+    }
+    cout << "(x+y)^n의 이항계수: " << endl;
+    for (int j = 0; j <= n; j++)
+    {
+        cout << setw(5) << pascal[n][j];
+    }
+    cout << endl;
+    for (int i = 0; i < n + 1; i++)
+    {
+        delete pascal[i];
+    }
+    delete[] pascal;
+    return 0;
 }
